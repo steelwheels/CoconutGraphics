62782f09fac000543c33e389122b4363e0394444
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-03-03 23:12:18 +0900

    Color: Update CNRGBtoColor function

--- Source/CNColorUtil.h
+++ Source/CNColorUtil.h
@@ -15,19 +15,19 @@
 #endif
 
 static inline KCColor *
-CNRGBtoColor(const struct CNRGB * col)
+CNRGBtoColor(const struct CNRGB col)
 {
 	KCColor * result ;
 #if TARGET_OS_IPHONE
-	result = [[UIColor alloc] initWithRed: col->red
-					green: col->green
-					 blue: col->blue
-					alpha: col->alpha] ;
+	result = [[UIColor alloc] initWithRed: col.red
+					green: col.green
+					 blue: col.blue
+					alpha: col.alpha] ;
 #else
-	result = [NSColor colorWithRed: col->red
-				 green: col->green
-				  blue: col->blue
-				 alpha: col->alpha] ;
+	result = [NSColor colorWithRed: col.red
+				 green: col.green
+				  blue: col.blue
+				 alpha: col.alpha] ;
 #endif
 	return result ;
 }
